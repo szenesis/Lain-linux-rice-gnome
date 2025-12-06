2697b095a1ffdc5259e6482bb6341cc24a465580
Szenesis <77949051+szenesis@users.noreply.github.com>
2025-12-06 02:21:27 -0600

    Delete dotfiles/waybar directory

--- dotfiles/waybar/scripts/shutdown/main.sh
+++
@@ -1,3 +0,0 @@
-mpv --start=0 --no-input-default-bindings --cursor-autohide=always --osc=no --fullscreen --on-all-workspaces ~/Videos/ending.mp4 &
-sleep 7.5;
-shutdown now
--- dotfiles/waybar/scripts/shutdown/opening.sh
+++
@@ -1,2 +0,0 @@
-sleep 5;
-mpv --no-input-default-bindings --cursor-autohide=always --osc=no --fullscreen --on-all-workspaces /home/flexander/Videos/opening.mp4;
